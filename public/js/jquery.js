07e644f7c5265a2b4c66c6789f3614bd7dd0e7f4
Niraj <niraj.mulji@gmail.com>
2016-03-31 09:07:23 -0500

    THis is our jquery exercise.

---
+++ public/js/jquery.js
@@ -0,0 +1,31 @@
+"use strict";
+
+//$("class-name");
+$(".codeup");
+
+//$("#id-name");
+$("#codeup");
+
+//for loop
+for (var i = 0; i < codeup.length; i++) {
+	codeup[i].style.color = "blue";
+}
+
+setTimeout(function() {
+
+var $codeup = $(".codeup");
+	console.log($codeup)
+	if (true) {
+		$codeup.css("color", "#a2a2a2");
+		$codeup.css("font-size", "24px");
+	}
+
+	$codeup.css ({
+		color: "#a2a2a2",
+		"font-size": "24px",
+	})
+}. 2000);
+
+//$("tagname");
+$("div");
+
