db2bdd1833923f97f3fa8b59e459cfae1f4101bd
Niraj <niraj.mulji@gmail.com>
2016-04-26 15:53:33 -0500

    This is my initial commit for my ping and pong exercise.

---
+++ public/ping.php
@@ -0,0 +1,21 @@
+<?php
+function pageController() {
+	$count = !isset($_GET['count']) ? 0 : $_GET['count'];
+	return ['count' => $count];
+}
+
+extract(pageController());
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+	<h1><?= $count ?></h1>
+	<a href="pong.php?count=<?php echo $count + 1 ?>">Hit</a>
+	<a href="pong.php?count=<?php echo $count = 0 ?>">Miss</a>
+</body>
+</html>
