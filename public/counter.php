13592dd9afe1563faace8219693dc3715bee664f
Niraj <niraj.mulji@gmail.com>
2016-04-26 15:22:29 -0500

    This is my counter exercise, initial commit.

---
+++ public/counter.php
@@ -0,0 +1,21 @@
+<?php
+function pageController() {
+	$count = !isset($_GET['count']) ? 0 : $_GET['count'];
+	return ['count' => $count];
+}
+
+extract(pageController());
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+	<h1><?= $count ?></h1>
+	<a href="?count=<?php echo $count + 1 ?>">Up</a>
+	<a href="?count=<?php echo $count - 1 ?>">Down</a>
+</body>
+</html>
