acd8252dce0f6af594ea66b075791ffc7744af06
Niraj <niraj.mulji@gmail.com>
2016-04-27 10:55:37 -0500

    This is my POST exercise, initial commit.

---
+++ public/login.php
@@ -0,0 +1,18 @@
+<?php
+var_dump($_POST);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <form method="POST">
+        <label>Username</label>
+        <input type="text" name="username"><br>
+        <label>Password</label>
+        <input type="text" name="password"><br>
+        <input type="submit">
+    </form>
+</body>
+</html>
