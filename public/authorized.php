61261544dcc332f5e8d3a2af24e2266964ec9740
Niraj <niraj.mulji@gmail.com>
2016-04-28 11:01:31 -0500

    This is my login, authorize, and logout completed exercise.

--- public/authorized.php
+++ public/authorized.php
@@ -1,3 +1,24 @@
+<?php
+
+function pageController() {
+
+$username = $_GET['username'];
+
+	if (isset($_SESSION['logged_in_user'])) {
+		echo $username;
+	} else {
+		header('Location: login.php');
+		exit;
+	}
+}
+
+if ($_POST) {
+	pageController();
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,5 +27,6 @@
 <body>
 	<h1>Authorized</h1>
 	<a href="login.php">Back</a>
+	<a href="logout.php">Logout</a>
 </body>
 </html>
---
+++ public/logout.php
@@ -0,0 +1,24 @@
+<?php
+
+// clear session data in memory & on disk and send user a new session cookie
+function clearSession()
+{
+    // clear $_SESSION array
+    session_unset();
+
+    // delete session data on the server and send the client a new cookie
+    session_regenerate_id(true);
+}
+
+// start the session (or resume an existing one)
+// this function must be called before trying to get or set any session data!
+session_start();
+
+clearSession();
+
+header('Location: login.php');
+
+
+// ...
+
+?>
