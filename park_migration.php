f950587c1ea2ce5f83487acdeb7fac360888ab6a
Niraj <niraj.mulji@gmail.com>
2016-05-13 10:24:48 -0500

    This is my parks migration exercies after I made the first exec statment to delete the national parks table.

--- park_migration.php
+++ park_migration.php
@@ -5,3 +5,7 @@ require_once 'parks_credentials.php';
 require_once 'parks_connect.php';
 
 echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
+
+$delete_table_national_parks = 'DROP TABLE IF EXISTS national_parks';
+
+$dbc->exec($delete_table_national_parks);
