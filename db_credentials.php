84ba46b14ce01300c5385ddc37f33565175fd868
Niraj <niraj.mulji@gmail.com>
2016-05-13 09:47:23 -0500

    This is my completed connecting db exercise.

---
+++ db_credentials.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_HOST','127.0.0.1');
+define('DB_NAME','employees');
+define('DB_USER','codeup');
+define('DB_PASS','ginidogg561');
+
+?>
--- pdo_test.php
+++ pdo_test.php
@@ -1,4 +1,7 @@
 <?php
 
-require 'db_connect.php';
+require_once 'db_credentials.php';
 
+require_once 'db_connect.php';
+
+echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
