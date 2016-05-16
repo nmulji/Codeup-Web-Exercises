767a7fe4766e40c52657a25de3fcbfa534eaf435
Niraj <niraj.mulji@gmail.com>
2016-05-16 10:00:52 -0500

    This is my parks_migration file after changing the code to also create an additional column called "Description".

--- park_migration.php
+++ park_migration.php
@@ -4,5 +4,22 @@ require_once 'parks_credentials.php';
 
 require_once 'parks_connect.php';
 
+echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
+
+$delete_table_national_parks = 'DROP TABLE IF EXISTS national_parks';
+
+$create_table_national_parks = 'CREATE TABLE national_parks (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    location VARCHAR(100) NOT NULL,
+    date_established DATE NOT NULL,
+    area_in_acres DOUBLE NOT NULL,
+    description VARCHAR(100) DEFAULT "NONE",
+    PRIMARY KEY (id)
+)';
+
+$dbc->exec($delete_table_national_parks);
+$dbc->exec($create_table_national_parks);
+
 require_once 'park_seeder.php';
 
